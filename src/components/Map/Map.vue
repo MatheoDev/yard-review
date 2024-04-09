6c037126bc5cf42bcbd8b80ffbc46e4b02ff86da
MatheoDev <matheo.deknuydt@gmail.com>
2024-04-10 00:06:03 +0200

    start here to perf 10,000 polygon on map divide by 100 layer

--- src/components/Map/Map.vue
+++ src/components/Map/Map.vue
@@ -1,5 +1,16 @@
 <template>
-    <div id="map" class="map-container">
+    <div class="test">
+        <div class="btn">
+            <button @click="addPolygon">Add polygon</button>
+            <button @click="hidePolygon">Hide polygon</button>
+            <button @click="showPolygon">Show polygon</button>
+            <button @click="removePolygon">Remove polygon</button>
+            <button @click="incrementBearing">+</button>
+            <button @click="decrementBearing">-</button>
+        </div>
+
+        <div id="map" class="map-container">
+        </div>
     </div>
 </template>
 
@@ -18,11 +29,71 @@ onMounted(() => {
     // add tile layer
     const tileLayer = L.tileLayer(configStore.tile.url, configStore.tile.options)
     tileLayer.addTo(map.value)
+
+    // add arround this coord 51.01137015793673, 2.1842228124579064 some polygon (rectangle 10px sur 20px en coord gps)
+    // const polygon = L.polygon([
+    //     [51.01137015793673, 2.1842228124579064],
+    //     [51.01137015793673, 2.1842228124579064 + 0.0001],
+    //     [51.01137015793673 + 0.0001, 2.1842228124579064 + 0.0001],
+    //     [51.01137015793673 + 0.0001, 2.1842228124579064]
+    // ]).addTo(map.value)
 })
+
+const addPolygon = () => {
+    for (let i = 0; i < 100; i++) {
+        const layerGroup = L.layerGroup().addTo(map.value)
+
+        for (let j = 0; j < 100; j++) {
+            L.polygon([
+                [51.01137015793673 + i * 0.0001, 2.1842228124579064 + j * 0.0001],
+                [51.01137015793673 + i * 0.0001, 2.1842228124579064 + j * 0.0001 + 0.0001],
+                [51.01137015793673 + i * 0.0001 + 0.0001, 2.1842228124579064 + j * 0.0001 + 0.0001],
+                [51.01137015793673 + i * 0.0001 + 0.0001, 2.1842228124579064 + j * 0.0001]
+            ]).addTo(layerGroup)
+        }
+    }
+}
+
+const hidePolygon = () => {
+    map.value.eachLayer((layer) => {
+        if (layer instanceof L.Polygon) {
+            layer.setStyle({ opacity: 0 })
+        }
+    })
+}
+
+const showPolygon = () => {
+    map.value.eachLayer((layer) => {
+        if (layer instanceof L.Polygon) {
+            layer.setStyle({ opacity: 1 })
+        }
+    })
+}
+
+const removePolygon = () => {
+    map.value.eachLayer((layer) => {
+        if (layer instanceof L.Polygon) {
+            map.value.removeLayer(layer)
+        }
+    })
+}
+
+const incrementBearing = () => {
+    map.value.setBearing(map.value.getBearing() + 1)
+}
+
+const decrementBearing = () => {
+    map.value.setBearing(map.value.getBearing() - 1)
+}
 </script>
 
 <style scoped>
 .map-container {
-    height: 50vh;
+    height: 80vh;
+}
+.btn {
+    display: flex;
+    gap: 10px;
+    margin: 10px;
 }
 </style>
